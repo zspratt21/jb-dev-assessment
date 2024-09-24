66a877a42598616fe1df0143a32c813e2039865d
Zak <zspratt21@gmail.com>
2024-09-24 11:53:05 +0800

    Post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,37 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ModelNotFoundException;
+use App\Exceptions\UnauthorizedException;
+use App\Http\Requests\PostCreateRequest;
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException as EloquentModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
+    /**
+     * @throws ModelNotFoundException
+     */
+    public function checkPostExists($id)
+    {
+        try {
+            Post::findOrFail($id);
+        } catch (EloquentModelNotFoundException $e) {
+            throw new ModelNotFoundException("Post with id $id not found");
+        }
+    }
+
+    /**
+     * @throws UnauthorizedException
+     */
+    public function checkPostUserId($id)
+    {
+        if ($id !== auth()->id()) {
+            throw new UnauthorizedException();
+        }
+
+        return true;
+    }
+
     public function index()
     {
         return Post::all();
     }
 
-    public function store(PostRequest $request)
+    public function store(PostCreateRequest $request)
     {
-        return Post::create($request->validated());
+        $existing_post = Post::where('title', $request->title)->first();
+        if ($existing_post) {
+            return response()->json(['error' => "Post with title `$request->title` already exists"], 400);
+        }
+
+        return $request->user()->posts()->create($request->validated());
     }
 
-    public function show(Post $post)
+    /**
+     * @throws ModelNotFoundException
+     */
+    public function show($id): JsonResponse
     {
-        return $post;
+        $this->checkPostExists($id);
+
+        return response()->json(Post::find($id));
     }
 
-    public function update(PostRequest $request, Post $post)
+    /**
+     * @throws UnauthorizedException
+     * @throws ModelNotFoundException
+     */
+    public function update(PostRequest $request, int $id): JsonResponse
     {
+        $this->checkPostExists($id);
+        $post = Post::find($id);
+        $this->checkPostUserId($post->user_id);
         $post->update($request->validated());
 
-        return $post;
+        return response()->json(Post::find($id));
     }
 
-    public function destroy(Post $post)
+    /**
+     * @throws UnauthorizedException
+     * @throws ModelNotFoundException
+     */
+    public function destroy(int $id)
     {
+        $this->checkPostExists($id);
+        $post = Post::find($id);
+        $this->checkPostUserId($post->user_id);
         $post->delete();
 
-        return response()->json();
+        return response()->json(['message' => "Post with $id deleted"]);
     }
 }
